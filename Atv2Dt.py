0fb55d446bc85de3eb9098e6d9b7015f7e3e27f1
Dantas <dantas.69.g@gmail.com>
2026-06-10 19:51:32 -0300

    Update Atv2Dt.py

--- Atv2Dt.py
+++ Atv2Dt.py
@@ -6,22 +6,36 @@ class Pessoa:
         self._altura = altura
     
     def __str__(self):
-        return f"Nome: {self._nome} altura: {self._altura}"
+        return f"Nome: {self._nome}\nAltura: {self._altura}\n"
     
     def __gt__(self, pessoa2):
         return self._altura > pessoa2._altura
 
     def __lt__(self, pessoa2):
         return self._altura < pessoa2._altura
-        
-x = int(input("Informe a quantidade de pessoas a serem cadastradas: "))
-pessoas = []
-for i in range(x):
-    nome = str(input("Informe o nome: "))
-    altura = float(input("Informe a altura: "))
-    pessoas.append(Pessoa(nome, altura))
-
-for pessoa in sorted(pessoas):
-    print(Pessoa)
-
-# A finalizar
+    
+pessoas = [] 
+while True:
+    y = str(input("Oque deseja fazer: ")).lower()
+    if y == "cadastrar":
+        x = int(input("Informe a quantidade de pessoas a serem cadastradas: "))
+        for i in range(x):
+            nome = str(input("Informe o nome: "))
+            altura = float(input("Informe a altura: "))
+            os.system("cls")
+            pessoas.append(Pessoa(nome, altura))
+    elif y == "listar":
+        for pessoa in sorted(pessoas, reverse=True):
+            print(pessoa)
+    elif y == "comparar":
+        p1 = str(input("Informe a primeira pessoa: "))
+        p2 = str(input("Informe a segunda pessoa: "))
+        for pessoa in pessoas:
+            if pessoa._nome == p1:
+                pessoa1 = pessoa
+            if pessoa._nome == p2:
+                pessoa2 = pessoa
+        print(p1 > p2)
+        print(p1 < p2)
+    elif y == "sair":
+        break
