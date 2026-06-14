d6332d4e2453c046574510fe02ed6947c24a4a30
Dantas <dantas.69.g@gmail.com>
2026-06-14 09:53:29 -0300

    Update Atv3Dt.py

--- Atv3Dt.py
+++ Atv3Dt.py
@@ -5,33 +5,28 @@ class Onibus:
     def __init__(self, placa, motorista):
         self._placa = placa
         self._motorista = motorista
-        self._numeroassentos = 0
-        self._assentos = []
+        self._numeroassentos = randint(15, 40)
+        self._assentos = [False] * self._numeroassentos
     
     def __len__(self):
-        self._numeroassentos = randint(15,40)
         return self._numeroassentos
     
     def __getitem__(self, indice):
-        if indice > self._numeroassentos:
+        if indice < 0 or indice >= self._numeroassentos:
             raise IndexError(f"Escolha um valor entre 0 e {len(self.assentos)}")
         else:
-            return self._assentos[indice].append(random.randint(True, False))
+            return self._assentos[indice]
     
     def __setitem__(self, indice, valor):
-        if isinstance(valor, bool) and indice < self._numeroassentos:
-            return self._assentos[indice].append(valor)
-        elif indice < 20:
-            raise TypeError(f"Valor deve ser booleano (True/False)")
+        if isinstance(valor, bool):
+            self._assentos[indice] = valor
+            return self._assentos[indice]
         else:
-            raise IndexError(f"Escolha um valor entre 0 e {len(self.assentos)}")
+            raise IndexError(f"Escolha um valor entre 0 e {len(self._assentos)}")
 
     def __str__(self):
-        for assento in range(0, self._numeroassentos):
-            if self._assentos[assento] == True:
-                vazio += 1
-            else:
-                vago += 1
+        vago = self._assentos.count(True)
+        vazio = self._assentos.count(False)
         return f"Placa do Onibus {self._placa}\nMotorista: {self._motorista}\nAssentos Totais: {self._numeroassentos}\nAssentos Vazios: {vazio}\nAssentos Vagos: {vago}"
         
 while True:
@@ -42,21 +37,16 @@ while True:
         onibus = Onibus(placa, motorista)
     elif x == "listar":
         os.system("cls")
-        len(onibus)
         print(onibus)
     
     elif x == "verifica":
         y = int(input("Informe o indice: "))
-        onibus[y]
+        print(onibus[y])
     
     elif x == "mudar":
         y = int(input("Informe o indice: "))
-        w = str(input("Informe o valor(True or False): ")).lower()
-        if w == "true":
-            onibus[y, w]
-        elif w == "false":
-            onibus[y, w]
+        if y < 0 or y > 40:
+            print("Indice inválido!")
         else:
-            print("Valor inváido!")
-
-# não finalizado
+            onibus[y] = True
+    # Finalizado!
