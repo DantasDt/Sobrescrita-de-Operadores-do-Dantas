ac8715bf9a627ca8ef87ec84cf9c525cfe9a3161
Dantas <dantas.69.g@gmail.com>
2026-06-15 19:51:16 -0300

    Update Atv4Dt.py

--- Atv4Dt.py
+++ Atv4Dt.py
@@ -19,10 +19,11 @@ class Mago(Personagem):
         self._mana = mana
 
     def __add__(self, valor):
-        return self._mana + valor
+        self._mana += valor
+        return self._mana
     
     def __sub__(self, valor):
-        self._mana - valor
+        self._mana = self._mana - valor
         if self._mana < 0:
             self._mana = 0
             return self._mana
@@ -30,32 +31,34 @@ class Mago(Personagem):
             return self._mana
     
     def __mul__(self, fator: float):
-        return self._mana * fator
+        self._mana *= fator
+        return self._mana
     
     def __truediv__(self, valor):
-        return self._mana / valor
+        self._mana = self._mana / valor
+        return self._mana
     
     def __str__(self):
-        super().__str__()
+        print(super().__str__())
         return f"Mana de {self._nome}: {self._mana}"
         
         
 
 class Barbaro(Personagem):
-    def __init__(self, nome, vida, estamina, furia):
+    def __init__(self, nome, vida, estamina, furia : bool):
         super().__init__(nome, vida)
         self._estamina = estamina
-        self._furia = False
+        self._furia = furia
     
     def __add__(self, valor):
         if self._furia == True:
-            self._estamina *= 1.5
+            self._estamina = self._estamina * 1.5
         else:
-            self._estamina += valor
+            self._estamina = self._estamina + valor
         return self._estamina
     
     def __sub__(self, valor):
-        self._estamina - valor
+        self._estamina = self._estamina - valor
         if self._estamina <= 0:
             self._estamina = 5
             self._furia = True
@@ -65,20 +68,97 @@ class Barbaro(Personagem):
     
     def __mul__(self, fator: float):
         if self._furia == True:
-            self._estamina *= fator
-            self._vida += 5
+            self._estamina = self._estamina * fator
+            self._vida = self._vida + 5
             return self._estamina, self._vida
         else:
-            self._estamina *= fator
+            self._estamina = self._estamina * fator
         return self._estamina
     
     def __truediv__(self, valor):
-        return self._estamina / valor
+        self._estamina = self._estamina / valor
+        return self._estamina
     
     def __str__(self):
-        super().__str__()
+        print(super().__str__())
         return f"Estamina de {self._nome}: {self._estamina}\nEstado: {self._furia}"
     
 while True:
     x = str(input("Oque deseja fazer? ")).lower()
-    # a finalizar
+    match x:
+        case "cadastrar":
+            os.system("cls")
+            nome = str(input("Informe o nome: "))
+            vida = int(input("Informe a vida: "))
+            classe = str(input("Informe a classe: ")).lower()
+            if classe == "mago":
+                mana = int(input("Informe a mana: "))
+                objM = Mago(nome, vida, mana)
+            elif classe == "barbaro":
+                estamina = int(input("Informe a estamina: "))
+                furia = bool(input("Informe a furia: "))
+                objB = Barbaro(nome, vida, estamina, furia)
+            else:
+                print("Classe Inválida!")
+        
+        case "listar":
+            os.system("cls")
+            match classe:
+                case "mago":
+                    print(objM)
+
+                case "barbaro":
+                    print(objB)
+        
+        case "somar":
+            os.system("cls")
+            valor = int(input("Informe o valor: "))
+            match classe:
+                case "mago":
+                    print(f"Mana de {objM._nome} + {valor} = {objM + valor}")
+
+                case "barbaro":
+                    print(f"Estamina de {objB._nome} + {valor} = {objB + valor}")
+        
+        case "subtrair":
+            os.system("cls")
+            valor = int(input("Informe o valor: "))
+            match classe:
+                case "mago":
+                    print(f"Mana de {objM._nome} - {valor} = {objM - valor}")
+
+                case "barbaro":
+                    print(f"Estamina de {objB._nome} - {valor} = {objB - valor}")
+        
+        case "multiplicar":
+            os.system("cls")
+            fator = int(input("Informe o valor: "))
+            match classe:
+                case "mago":
+                    print(f"Mana de {objM._nome} * {fator} = {objM * fator}")
+
+                case "barbaro":
+                    print(f"Estamina de {objB._nome} * {fator} = {objB * fator}")
+        
+        case "dividir":
+            os.system("cls")
+            valor = int(input("Informe o valor: "))
+            match classe:
+                case "mago":
+                    print(f"Mana de {objM._nome} / {valor} = {objM / valor}")
+
+                case "barbaro":
+                    print(f"Estamina de {objB._nome} / {valor} = {objB / valor}")
+
+        case "enfurecer":
+            os.system("cls")
+            match classe:
+                case "mago":
+                    print("Essa classe não pode ficar enfurecida!")
+
+                case "barbaro":
+                    objB._furia = True
+        case "sair":
+            os.system("cls")
+            break
+# Quase finalizado
